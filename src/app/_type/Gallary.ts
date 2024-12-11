721e5e1f4f9141a5e485a92921ab497d3806eb34
Tomoe <osmanthus31172@gmail.com>
2024-12-11 22:08:17 +0900

    ギャラリーグループバックエンド実装

--- src/app/_type/Gallary.ts
+++
@@ -1,9 +0,0 @@
-export interface Gallary {
-  id: number;
-  title: string;
-  content: string;
-  createdAt: string;
-  galleryGroupTitle: string;
-  galleryGroupId: number;
-  thumbnailImageKey: string;
-}
