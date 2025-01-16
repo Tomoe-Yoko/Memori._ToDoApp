61da8c03fc962b3d8cd8e19e74d0472be7bda6e0
Tomoe <osmanthus31172@gmail.com>
2025-01-16 22:49:34 +0900

    favicon追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,10 @@ export default function RootLayout({
       <Head>
         <title>Memori Todo App</title>
         <meta name="description" content="todoアプリで快適な生活を" />
+        {/* ファビコン関連 */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </Head>
       <body
         className={`${jost.className} ${
